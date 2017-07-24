c662f2f91edb1c14cc662a8415d2e161a6f0df87
TenPlus1 <kinsellaja@yahoo.com>
2017-07-24 20:31:01 +0100

    add lb custom function

--- lucky_block.lua
+++ lucky_block.lua
@@ -1,11 +1,22 @@
 
 -- add lucky blocks
 
+local function growy(pos, player)
+
+	local dpos = minetest.find_node_near(pos, 1, "group:soil")
+
+	if dpos then
+		bonemeal:on_use(dpos, 5)
+	end
+end
+
+
 if minetest.get_modpath("lucky_block") then
 
 lucky_block:add_blocks({
 	{"lig"},
 	{"dro", {"bonemeal:mulch", "bonemeal:bonemeal", "bonemeal:fertiliser"}, 10},
+	{"cus", growy},
 })
 
 end -- END IF
