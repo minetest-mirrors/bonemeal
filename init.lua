c621448df3fb4317e1986fd751335ca8e7f9f498
TenPlus1 <kinsellaja@yahoo.com>
2019-10-05 10:12:10 +0100

    nil checks for grass and decoration lists

--- init.lua
+++ init.lua
@@ -266,10 +266,12 @@ local function check_soil(pos, nodename, strength)
 
 		if math.random(1, 5) == 5 then
 			-- place random decoration (rare)
-			nod = decor[math.random(1, (#decor or 1))] or ""
+			local dnum = #decor or 1
+			nod = decor[math.random(1, dnum)] or ""
 		else
 			-- place random grass (common)
-			nod = #grass > 0 and grass[math.random(1, #grass)] or ""
+			local dgra = #grass or 1
+			nod = #grass > 0 and grass[math.random(1, dgra)] or ""
 		end
 
 		if nod and nod ~= "" then
